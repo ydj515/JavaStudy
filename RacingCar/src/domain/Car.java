35b17da2425b41671edaefbd00b102078b79c17e
ydj515 <ydj515@hanmail.net>
2020-08-05 00:14:09 +0900

    domain

---
+++ RacingCar/src/domain/Car.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class Car {
+
+    private final String name;
+    private int position = 1;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    /* Stream에서 사용하려고 만듬 */
+    public static Car of(String name) {
+        return new Car(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void movingOneStep(int position) {
+        this.position += position;
+    }
+}
