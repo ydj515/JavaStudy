d92466133a3ed2f221ef87248a041ac9b5c74241
ydj515 <ydj515@hanmail.net>
2020-08-21 01:24:09 +0900

    [LadderGame] main

---
+++ LadderGame/src/Main.java
@@ -0,0 +1,11 @@
+import controller.LadderGameController;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		LadderGameController controller = new LadderGameController();
+		controller.run();
+	}
+
+}
