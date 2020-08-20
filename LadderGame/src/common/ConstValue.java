089868a41cd48c1fe78840d08b25d93daed6de9d
ydj515 <ydj515@hanmail.net>
2020-08-21 01:24:35 +0900

    [LadderGame] const value

---
+++ LadderGame/src/common/ConstValue.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.util.Scanner;
+
+public class ConstValue {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static final String SEPERATOR = ",";
+
+
+}
