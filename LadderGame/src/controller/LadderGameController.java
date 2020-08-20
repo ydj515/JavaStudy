27f530d70c2493515d8f8107a78ebca21b6174fa
ydj515 <ydj515@hanmail.net>
2020-08-21 01:24:46 +0900

    [LadderGame] controller

---
+++ LadderGame/src/controller/LadderGameController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.*;
+import java.util.stream.*;
+
+import model.Ladder;
+import model.Player;
+import view.LadderGameInputView;
+import view.LadderrGameOutputView;
+
+public class LadderGameController {
+
+	public void run() {
+
+		List<String> playersNames = LadderGameInputView.inputPlayers();
+        List<Player> players = playersNames.stream().map(Player::new).collect(Collectors.toList());
+
+		int height = LadderGameInputView.inputLadderHigh();
+		Ladder ladder = new Ladder(height, players.size());
+
+		LadderrGameOutputView.printProcess(height, playersNames, ladder);
+	}
+
+}
