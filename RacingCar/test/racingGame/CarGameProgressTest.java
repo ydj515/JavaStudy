4f6dd302700383cc8c3e7f8cf3a378d35905013f
ydj515 <ydj515@hanmail.net>
2020-08-30 00:39:04 +0900

    [RacingCar] test code

--- RacingCar/test/racingGame/CarGameProgressTest.java
+++ RacingCar/test/racingGame/CarGameProgressTest.java
@@ -1,16 +1,33 @@
 package racingGame;
 
+import common.ConstValue;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CarGameProgressTest {
 
-    @Test
-    public void startGame() {
+    @Before
+    public void setUp() {
+        System.out.println("===Set up===");
     }
 
     @Test
-    public void testStartGame() {
+    public void calculatePositionTest() {
+        assertEquals("-", calculatePosition(1));
+        assertEquals("",calculatePosition(0));
+        assertEquals("---",calculatePosition(3));
+    }
+
+    private String calculatePosition(int n) {
+        String countPosition = "";
+
+        for (int i = 0; i < n; i++) {
+            countPosition += ConstValue.POSITION_STRING;
+        }
+
+        return countPosition;
     }
 }
--- RacingCar/test/view/InputViewTest.java
+++ RacingCar/test/view/InputViewTest.java
@@ -1,28 +1,26 @@
 package view;
 
 import common.ConstValue;
+import exception.NotPositiveNumberException;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
+import static java.lang.System.exit;
 import static org.junit.Assert.assertEquals;
 
 public class InputViewTest {
 
-    private Scanner scanner;
-
     @Before
     public void setUp() {
         System.out.println("===Set up===");
-        scanner = new Scanner(System.in);
     }
 
     @Test
-    public void inputCarName() {
+    public void inputCarNameTest() {
         assertEquals(Arrays.asList("a", "b", "c"), inputCarNameConvertLogic("a,b,c"));
     }
 
@@ -31,6 +29,46 @@ public class InputViewTest {
     }
 
     @Test
-    public void inputTryNumAndReturnNum() {
+    public void validIsNumberTest() {
+        assertEquals(-1, validIsNumber("-1"));
+        assertEquals(-1, validIsNumber("abc"));
+        assertEquals(-1, validIsNumber(" "));
+        assertEquals(12, validIsNumber("12"));
+        assertEquals(-0, validIsNumber("0"));
     }
+
+    private int validIsNumber(String inputString) {
+
+        int inputNumber = -1;
+        try {
+            inputNumber = Integer.parseInt(inputString);
+        } catch (NumberFormatException e) {
+            System.out.println(ConstValue.NUMBER_FORMAT_EXCEPTION_MESSAGE);
+//            exit(0);
+        }
+
+        return inputNumber;
+    }
+
+    @Test
+    public void validIsPositiveNumberTest() {
+        assertEquals(1, validIsPositiveNumber(1));
+        assertEquals(-1, validIsPositiveNumber(-1));
+        assertEquals(-1, validIsPositiveNumber(-500));
+        assertEquals(30, validIsPositiveNumber(30));
+        assertEquals(-1, validIsPositiveNumber(0));
+
+    }
+
+    private int validIsPositiveNumber(int num) {
+        try {
+            NotPositiveNumberException.checkPositiveNumber(num);
+        } catch (NotPositiveNumberException e) {
+            System.out.println(ConstValue.INPUT_TRY_AGAIN_QUESTION);
+            return -1;
+//            exit(0);
+        }
+        return num;
+    }
+
 }
