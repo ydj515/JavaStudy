b1a6496fc7bb235c64627e2d8fabe18b0a8004d1
ydj515 <ydj515@hanmail.net>
2020-09-05 22:03:58 +0900

    [LottoGame] Random number 생성기 추가

--- LottoGame/src/common/RandomLottoNumberGenerator.java
+++ LottoGame/src/common/RandomLottoNumberGenerator.java
@@ -5,17 +5,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class RandomLottoNumberGenrator {
-
-    private final int MAX_SIZE = 6;
-    private final int MAX_RANDOM_NUMBER_LIMIT = 45;
+public class RandomLottoNumberGenerator {
 
     public List<Integer> createLottoNumbers() {
 
         Set<Integer> randomNumberSet = new HashSet<>();
 
-        while (randomNumberSet.size() != MAX_SIZE) {
-            int randomNumber = (int) (Math.random() * MAX_RANDOM_NUMBER_LIMIT + 1);
+        while (randomNumberSet.size() != ConstantValue.MAX_SIZE) {
+            int randomNumber = (int) (Math.random() * ConstantValue.MAX_RANDOM_NUMBER_LIMIT + 1);
             randomNumberSet.add(randomNumber);
             randomNumberSet.remove(0);
         }
